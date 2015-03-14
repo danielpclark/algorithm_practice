f3ba01438775b6c68f6fe0b677abdbd816651b5d
Daniel P. Clark <6ftdan@gmail.com>
2015-03-14 07:26:03 -0400

    Working sieve of Eratosthenes

--- lib/Chapter1/section1.rb
+++ lib/Chapter1/section1.rb
@@ -24,8 +24,33 @@ module Chapter1
       end
     end
 
-    def sieve_of_Eratosthenes
+    def sieve_of_Eratosthenes(n)
+      a = []
+            
+      (2..n) .each do |p|
+        a[p] = p
+      end
 
+      (2..Math.sqrt(n).floor).each do |p|
+        if a[p] != 0
+          j = p * p
+          while j <= n do
+            a[j] = 0
+            j = j + p
+          end
+        end 
+      end
+
+      l = []
+      i = 0
+      
+      (2..n).each do |p|
+        if a[p] != 0
+          l[i] = a[p]
+          i = i + 1
+        end
+      end
+      return l
     end
   end
 end
