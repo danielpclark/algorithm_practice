519c64d0d5b2ba1fb95522cc7b630b436f167657
Daniel P. Clark <6ftdan@gmail.com>
2015-03-13 20:18:12 -0400

    Named files by capter and section.

---
+++ lib/Chapter1/section1.rb
@@ -0,0 +1,32 @@
+module Chapter1
+  module Section1
+    def euclid(m,n)
+      raise "Invalid input." unless (m>0 && n>=0 && m>n)
+      while n != 0 do
+        r = m % n
+        m = n
+        n = r
+      end
+      return m
+    end
+
+    def min(m,n,c=nil)
+      unless c
+        raise "Invalid input." unless (m>0 && n>0)
+        m,n = [m,n].sort { n <=> m }
+        c = n
+      end
+      if (m % c == 0) && (n % c == 0)
+        return c
+      else
+        c = c - 1
+        min(m,n,c)
+      end
+    end
+
+    def sieve_of_Eratosthenes
+
+    end
+  end
+end
+
--- test/Chapter1/section1_test.rb
+++ test/Chapter1/section1_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'minitest/autorun'
 
 describe Chapter1 do
-  include Chapter1
+  include Chapter1::Section1
 
   it "#euclid" do
     euclid(60,24).must_equal 12
@@ -12,6 +12,10 @@ describe Chapter1 do
 
   it "#min" do
     min(60, 24).must_equal 12
+  end
+
+  it "#sieve_of_Eratosthenes" do
+    sieve_of_Eratosthenes(100).must_equal [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
   end
 
 end
